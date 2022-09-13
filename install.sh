75313ed2f4a295683131c82d7f971fa3eef27e1c
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-09-13 16:21:15 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo amazon-linux-extras install epel
 echo epel is installed
 
-for package in cowsay jq tmux epel-release sl zsh git zshrc tldr gcc
+for package in cowsay jq tmux epel-release sl zsh git tldr gcc
 do
 	sudo yum install -y $package
 	echo $package was installed | cowsay
@@ -14,9 +14,14 @@ done
 #install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+curl https://raw.githubusercontent.com/kris-classes/restart/main/tia_zshrc > $HOME/.zshrc
+echo retrieved .zshrc | cowsay
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custo$
+echo zsh-autosuggestions was installed | cowsay
+
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh -s -- -y
-
 echo rust was installed | cowsay
 
 source $HOME/.cargo/env
