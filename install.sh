b133eb27b867650e122c83571b4b69405491b284
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-09-13 15:49:51 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@
 sudo amazon-linux-extras install epel
 echo epel is installed
 
-for package in cowsay jq tmux epel-release sl zsh git zshrc
+for package in cowsay jq tmux epel-release sl zsh git zshrc cc
 do
 	sudo yum install -y $package
 	echo $package was installed | cowsay
 done
+
+source $HOME/.cargo/env
 
 #install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
