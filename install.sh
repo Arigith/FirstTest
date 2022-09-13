e6e544d402f46f2d6863e802e40a957cd9c3bec9
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-09-13 15:44:22 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,14 @@
 sudo amazon-linux-extras install epel
 echo epel is installed
 
-for package in cowsay jq tmux amazon-linux-extras epel-release sl
+for package in cowsay jq tmux amazon-linux-extras epel-release sl zsh git zshrc
 do
 	sudo yum install -y $package
 	echo $package was installed | cowsay
 done
 
-# Install rust
+#install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
