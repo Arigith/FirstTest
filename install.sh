f4d238371492d629a363ca332b68e748bdcad731
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-09-13 15:24:26 +1200

    Updated

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#The purpose of this program is to automate the installation of packages
+
+# The purpose of this program is to automate the installation of packages
+
 for packakage in cowsay jq tmux amazon-linux-extras epel-release sl
 do
 	sudo yum install -y $package
- 	echo $package was installed | cowsay
+	echo $package was installed | cowsay
 done
 
 # Install rust
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
