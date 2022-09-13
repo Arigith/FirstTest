d27e629f314f5b1fe35ce2ee610029879c096255
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2022-09-13 16:11:26 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ done
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -y | sh -s -- -y
 
 source $HOME/.cargo/env
 
